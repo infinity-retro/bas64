10335a9c0f93125becc43f4d21d462be1765fbf7
Finity <retrywebster@gmail.com>
2025-06-05 22:03:48 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
+use base64::{engine::general_purpose::STANDARD, Engine};
+    
+pub fn encode<T: AsRef<[u8]>>(input: T) -> String {
+    STANDARD.encode(input)
+}
 
+pub fn decode<T: AsRef<[u8]>>(input: T) -> Result<Vec<u8>, base64::DecodeError> {
+    STANDARD.decode(input)
+}
